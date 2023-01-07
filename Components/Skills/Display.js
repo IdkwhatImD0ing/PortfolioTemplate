e37a4684939e4414dfbdd8b8d933adce1dcb68ae
IdkwhatImD0ing <billzhangsc@gmail.com>
2023-01-06 18:33:18 -0800

    Centered skills page

--- Components/Skills/Display.js
+++ Components/Skills/Display.js
@@ -51,7 +51,13 @@ export default function Display() {
         paddingTop: '10vh',
       }}
     >
-      <Stack direction="column" width="80%" spacing={5}>
+      <Stack
+        direction="column"
+        width="80%"
+        spacing={5}
+        justifyContent="flex-start"
+        alignItems="center"
+      >
         <Typography variant="h3" color="white" align="center">
           Skills
         </Typography>
