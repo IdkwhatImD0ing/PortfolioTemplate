9e3b7e4d340e53af9e284c1d1e901aa01d5220c6
Bill Zhang <billzhangsc@gmail.com>
2023-01-15 03:16:25 -0800

    messing around with animations

--- Components/HomePage/Hero.js
+++ Components/HomePage/Hero.js
@@ -1,7 +1,16 @@
 import {Box, Stack, Typography} from '@mui/material';
 import Typer from './Typewriter';
+import {animated, useSpring} from '@react-spring/web';
 
 export default function Hero() {
+  const top = useSpring({
+    from: {y: -100, opacity: 0},
+    to: {y: 0, opacity: 1},
+  });
+  const bot = useSpring({
+    from: {y: 100, opacity: 0},
+    to: {y: 0, opacity: 1},
+  });
   return (
     <Box
       height="100vh"
@@ -19,43 +28,67 @@ export default function Hero() {
       }}
     >
       <Stack maxWidth="80vw" direction="column" alignItems="center">
-        <Typography
-          variant="h5"
-          align="center"
-          sx={{
-            color: 'pink',
-            mb: 2,
+        <animated.div
+          style={{
+            ...top,
           }}
         >
-          Hello There! I&apos;m Bill Zhang
-        </Typography>
-        <Typography
-          align="center"
-          sx={{
-            color: 'white',
-            fontFamily: 'sans-serif',
-            fontWeight: '800',
-            fontSize: '60px',
+          <Typography
+            variant="h5"
+            align="center"
+            sx={{
+              color: 'pink',
+              mb: 2,
+            }}
+          >
+            Hello There! I&apos;m Bill Zhang
+          </Typography>
+        </animated.div>
+        <animated.div
+          style={{
+            ...top,
           }}
         >
-          I&apos;m a passionate
-        </Typography>
-        <Typer />
-        <Typography
-          variant="h5"
-          align="center"
-          sx={{
-            color: 'white',
-            mt: 5,
+          <Typography
+            align="center"
+            sx={{
+              color: 'white',
+              fontFamily: 'sans-serif',
+              fontWeight: '800',
+              fontSize: '60px',
+            }}
+          >
+            I&apos;m a passionate
+          </Typography>
+        </animated.div>
+        <animated.div
+          style={{
+            ...top,
           }}
         >
-          I am a Third Year Undergrad at UC Santa Cruz, studying Computer
-          Science with a focus on Machine Learning and Full-Stack Web
-          Development. I am a Full-Stack Developer with experience in React,
-          NextJs, NodeJs, ExpressJs, Flask, and PostGreSQL. I also have
-          experience in Applied Machine Learning through personal projects and
-          Kaggle Competitions.
-        </Typography>
+          <Typer />
+        </animated.div>
+        <animated.div
+          style={{
+            ...bot,
+          }}
+        >
+          <Typography
+            variant="h5"
+            align="center"
+            sx={{
+              color: 'white',
+              mt: 5,
+            }}
+          >
+            I am a Third Year Undergrad at UC Santa Cruz, studying Computer
+            Science with a focus on Machine Learning and Full-Stack Web
+            Development. I am a Full-Stack Developer with experience in React,
+            NextJs, NodeJs, ExpressJs, Flask, and PostGreSQL. I also have
+            experience in Applied Machine Learning through personal projects and
+            Kaggle Competitions.
+          </Typography>
+        </animated.div>
       </Stack>
     </Box>
   );
