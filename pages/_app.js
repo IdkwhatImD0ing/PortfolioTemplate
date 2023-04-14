93a1c0c3c181272132ed49f24d49e961e68620a1
IdkwhatImD0ing <billzhangsc@gmail.com>
2023-04-13 19:27:10 -0700

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {ThemeProvider} from '@mui/material';
-import {themeOptions} from '../Components/theme';
-import './App.scss';
-import Matrix from '../Components/HomePage/Matrix';
-import Topbar from '../Components/Topbar/Topbar';
-import Botbar from '../Components/Botbar';
-import {WidthContextProvider} from '../Components/page';
+import React, {useState, useEffect} from 'react'
+import {ThemeProvider} from '@mui/material'
+import {themeOptions} from '../Components/theme'
+import './App.scss'
+import Matrix from '../Components/HomePage/Matrix'
+import Topbar from '../Components/Topbar/Topbar'
+import Botbar from '../Components/Botbar'
+import {WidthContextProvider} from '../Components/page'
+import {Analytics} from '@vercel/analytics/react'
 
 function MyApp({Component, pageProps}) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    setLoading(false);
-  }, []);
+    setLoading(false)
+  }, [])
   return loading ? (
     <div className="Loader-wrapper">
       <div className="Loader" />
@@ -24,8 +25,9 @@ function MyApp({Component, pageProps}) {
         <Topbar />
         <Botbar />
       </WidthContextProvider>
+      <Analytics />
     </ThemeProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
